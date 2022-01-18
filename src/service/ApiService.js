31f7b7e2036a0083a2b53384044574fc72bfd8f0
JunHwan Kim <nefertiri972@gmail.com>
2022-01-19 00:35:16 +0900

    Add saving access token
    - 로그인 시 받은 토큰을 로컬 스토리지에 저장
    - 모든 API 요청시 토큰이 존재하는 경우 토큰을 헤더에 추가

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,13 +1,23 @@
 import {API_BASE_URL} from "../app-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
+
+  let headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  const accessToken = localStorage.getItem(ACCESS_TOKEN);
+  if (accessToken && accessToken !== null) {
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
+
   let options = {
-    headers: new Headers({
-      "Content-Type": "application/json",
-    }),
+    headers: headers,
     url: API_BASE_URL + api,
     method: method,
   };
+
   if (request) {
     options.body = JSON.stringify(request);
   }
@@ -31,6 +41,7 @@ export function signin(userDto) {
   return call("/auth/signin", "POST", userDto)
     .then((response) => {
       if (response.token) {
+        localStorage.setItem(ACCESS_TOKEN, response.token);
         window.location.href = "/";
       }
     });
